d39a4fc11554cffdfc01db510a7e6314aa956034
s1270146 <s1270146@gmail.com>
2022-10-30 22:42:18 +0900

    firebase 接続

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:calendar/login/login.dart';
 import 'package:calendar/plan/plan_model.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
